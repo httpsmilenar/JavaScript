4ef5a42c0057d193abefe311fdd259c6b6a19aa4
httpsmilenar <millarodrigues304@gmail.com>
2022-05-09 20:43:37 -0300

    first commit

---
+++ aula04/aula11/exercise008.js
@@ -0,0 +1,6 @@
+var vel = 12
+console.log(`your car speed is ${vel}km/h`)
+if (vel > 60) {
+    console.log('you have passed the permitted speed. FINED!')
+}
+console.log('always drive wearing a seat belt!')
---
+++ aula04/aula11/exercise009.js
@@ -0,0 +1,7 @@
+var nation = 'EUA'
+console.log(`living in ${nation}`)
+if (nation == 'Brasil') {
+    console.log('you are Brasilian!')
+} else {
+    console.log('you are foreign!')
+}
